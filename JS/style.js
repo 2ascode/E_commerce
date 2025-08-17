caf710f9fe677903e897f3e55fa018c384e67c69
azigsoton <azigsoton@gmail.com>
2025-08-17 18:04:24 +0100

    annunaltion du commit

--- JS/style.js
+++ JS/style.js
@@ -251,23 +251,23 @@ iphoneStock.initShoppingCartList();
 iphoneStock.initCount();
 
 const pathname = window.location.pathname;
-if (pathname == "admin.html") {
+if (pathname == "/admin.html") {
   iphoneStock.addProduct();
   iphoneStock.displayCouunt();
 }
 
-if (pathname == "shop.html") {
+if (pathname == "/shop.html") {
   iphoneStock.displayCouunt();
   iphoneStock.displayIos();
   iphoneStock.addToShoppingCartList();
 }
 
-if (pathname == "index.html") {
+if (pathname == "/index.html") {
   iphoneStock.displayCouunt();
   iphoneStock.addToShoppingCartList();
 }
 
-if (pathname == "shopping_cart.html") {
+if (pathname == "/shopping_cart.html") {
   iphoneStock.displayCouunt();
   iphoneStock.displayShoppingCartList();
   iphoneStock.initShoppingCartList()
